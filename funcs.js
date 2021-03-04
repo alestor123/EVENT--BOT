e026c2971cdd2268087561eba3717b65bd0ccf3f
alestor123 <alestoraldous@gmail.com>
2021-03-04 22:12:26 +0530

    funcs js code

---
+++ funcs.js
@@ -0,0 +1,19 @@
+ray = require('x-ray')();
+
+async function getEvents() {
+var eventss = [];
+var data = await ray('https://mlh.io/events', '.event', [{title: '.event-link@title',date:'.event-date',url: '.event-link@href',date : 'meta@content' , img:'img@src'}]).then(res => res) 
+data.forEach(events => {
+events['isMlh']=events.url.includes('https://organize.mlh.io/participants/events/')
+eventss.push(events)
+})
+return eventss
+}
+function getWeekend() {
+    var date = new Date(), 
+    lastday = date.getDate() - (date.getDay() - 1) + 6;
+    snd = date.getDate() - (date.getDay() - 0) + 6,
+    tt = date.getDate() - (date.getDay() + 1) + 6;
+    
+    return [new Date(date.setDate(lastday)).toISOString().split('T')[0],new Date(date.setDate(snd)).toISOString().split('T')[0],new Date(date.setDate(tt)).toISOString().split('T')[0]]
+}
